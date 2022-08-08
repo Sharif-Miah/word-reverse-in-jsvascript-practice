c4b3e076cc1c5ab7bf21be85736f5d363d5f7e1c
farhan sharif <farhansharif1410@gmail.com>
2022-08-07 21:36:52 -0230

    word reverse added

--- word-reverse.js
+++ word-reverse.js
@@ -1,7 +1,15 @@
 
 function wordReverse(str) {
-    let wordLenght = str.split(' ')
-    console.log(wordLenght);
+    let wordLenght = str.split(' ');
+    const array = [];
+    // ['This', 'is', 'a', 'word']
+
+    for (i = wordLenght.length - 1; i >= 0; i--) {
+        const element = wordLenght[i];
+        array.push(element);
+    }
+    const reverse = array.join(' ')
+    return reverse;
 }
 
 const thisWord = 'This is a word'
